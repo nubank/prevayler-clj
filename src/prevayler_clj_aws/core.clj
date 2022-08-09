e347adcb569b337833fda4b9979342146c09a3a5
Rui Fernando Hayashi <rfhayashi@gmail.com>
2022-08-09 16:52:29 -0300

    refactorings

--- src/prevayler_clj_aws/core.clj
+++ src/prevayler_clj_aws/core.clj
@@ -22,32 +22,32 @@
       base64/decode-bytes
       nippy/thaw))
 
-(defn- snapshot-exists? [s3-client bucket snapshot-path]
-  (->> (util/aws-invoke s3-client {:op :ListObjects
-                                   :request {:Bucket bucket
-                                             :Prefix snapshot-path}})
+(defn- snapshot-exists? [s3-cli bucket snapshot-path]
+  (->> (util/aws-invoke s3-cli {:op :ListObjects
+                                :request {:Bucket bucket
+                                          :Prefix snapshot-path}})
        :Contents
        (some #(= snapshot-path (:Key %)))))
 
-(defn- read-snapshot [s3-client bucket snapshot-path]
-  (if (snapshot-exists? s3-client bucket snapshot-path)
-    (-> (util/aws-invoke s3-client {:op :GetObject
-                                    :request {:Bucket bucket
-                                              :Key snapshot-path}})
+(defn- read-snapshot [s3-cli bucket snapshot-path]
+  (if (snapshot-exists? s3-cli bucket snapshot-path)
+    (-> (util/aws-invoke s3-cli {:op :GetObject
+                                 :request {:Bucket bucket
+                                           :Key snapshot-path}})
         :Body
         unmarshal)
     {:partkey 0}))
 
-(defn- save-snapshot! [s3-client bucket snapshot-path snapshot]
-  (util/aws-invoke s3-client {:op :PutObject
-                              :request {:Bucket bucket
-                                        :Key snapshot-path
-                                        :Body (marshal snapshot)}}))
+(defn- save-snapshot! [s3-cli bucket snapshot-path snapshot]
+  (util/aws-invoke s3-cli {:op :PutObject
+                           :request {:Bucket bucket
+                                     :Key snapshot-path
+                                     :Body (marshal snapshot)}}))
 
-(defn- read-items [client table partkey page-size]
+(defn- read-items [dynamo-cli table partkey page-size]
   (letfn [(read-page [exclusive-start-key]
             (let [result (util/aws-invoke
-                          client
+                          dynamo-cli
                           {:op :Query
                            :request {:TableName table
                                      :KeyConditionExpression "partkey = :partkey"
@@ -62,37 +62,36 @@
                  []))))]
     (read-page {:order {:N "0"} :partkey {:S (str partkey)}})))
 
-(defn- restore-events! [handler state-atom client table partkey page-size]
-  (let [items (read-items client table partkey page-size)]
+(defn- restore-events! [dynamo-cli handler state-atom table partkey page-size]
+  (let [items (read-items dynamo-cli table partkey page-size)]
     (doseq [[timestamp event] items]
       (swap! state-atom handler event timestamp))))
 
-(defn- write-event! [client table partkey order event]
-  (util/aws-invoke client {:op :PutItem
-                           :request {:TableName table
-                                     :Item {:partkey {:S (str partkey)}
-                                            :order {:N (str order)}
-                                            :content {:B (marshal event)}}}}))
+(defn- write-event! [dynamo-cli table partkey order event]
+  (util/aws-invoke dynamo-cli {:op :PutItem
+                               :request {:TableName table
+                                         :Item {:partkey {:S (str partkey)}
+                                                :order {:N (str order)}
+                                                :content {:B (marshal event)}}}}))
 
 (defn prevayler! [{:keys [initial-state business-fn timestamp-fn aws-opts]
                    :or {initial-state {}
                         timestamp-fn #(System/currentTimeMillis)}}]
-  (let [dynamodb-cli (or (:dynamodb-client aws-opts) (aws/client {:api :dynamodb}))
-        s3-cli (or (:s3-client aws-opts) (aws/client {:api :s3-client}))
-        table (:dynamodb-table aws-opts)
-        snapshot-path (or (:snapshot-path aws-opts) "snapshot")
-        bucket (:s3-bucket aws-opts)
-        {state :state old-partkey :partkey} (read-snapshot s3-cli bucket snapshot-path)
+  (let [{:keys [dynamodb-client s3-client dynamodb-table snapshot-path s3-bucket page-size]
+         :or {dynamodb-client (aws/client {:api :dynamodb})
+              s3-client (aws/client {:api :s3})
+              snapshot-path "snapshot"
+              page-size 1000}} aws-opts
+        {state :state old-partkey :partkey} (read-snapshot s3-client s3-bucket snapshot-path)
         state-atom (atom (or state initial-state))
         new-partkey (inc old-partkey)
-        order-counter-atom (atom 0)
-        page-size (or (:page-size aws-opts) 1000)]
+        order-atom (atom 0)]
 
-    (restore-events! business-fn state-atom dynamodb-cli table old-partkey page-size)
+    (restore-events! dynamodb-client business-fn state-atom dynamodb-table old-partkey page-size)
 
     ; since s3 update is atomic, if saving snapshot fails next prevayler will pick the previous state
     ; and restore events from the previous partkey
-    (save-snapshot! s3-cli bucket snapshot-path {:state @state-atom :partkey new-partkey})
+    (save-snapshot! s3-client s3-bucket snapshot-path {:state @state-atom :partkey new-partkey})
 
     (reify
       Prevayler
@@ -100,10 +99,10 @@
         (locking this ; (I)solation: strict serializability.
           (let [timestamp (timestamp-fn)
                 new-state (business-fn @state-atom event timestamp)] ; (C)onsistency: must be guaranteed by the handler. The event won't be journalled when the handler throws an exception.)
-            (write-event! dynamodb-cli table new-partkey (swap! order-counter-atom inc) [timestamp event]) ; (D)urability
+            (write-event! dynamodb-client dynamodb-table new-partkey (swap! order-atom inc) [timestamp event]) ; (D)urability
             (reset! state-atom new-state)))) ; (A)tomicity
       (timestamp [_] (timestamp-fn))
 
       IDeref (deref [_] @state-atom)
 
-      Closeable (close [_] (aws/stop dynamodb-cli)))))
+      Closeable (close [_] (aws/stop dynamodb-client)))))
